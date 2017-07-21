cb8fe55e5b363f5f5464a23d004a7912fa9584a6
Mathieu <mathieu@MacBook-Pro-de-Mathieu.local>
2017-07-21 15:27:01 +0200

    add compteur break sesssion

--- js/main.js
+++ js/main.js
@@ -1,18 +1,27 @@
 
 var timer = null
-var value = 100;
-$(".cercle").text(value);
+var bonjour =1
+var value = $(".resdroite").text()*60
+$(".cercle").text(sec(value));
 $(".cercle").click(function() {
  if (timer !== null) return;
  timer = setInterval(function () {
      value = value-1;
-     $(".cercle").text(value);
+     $(".cercle").text(sec(value));
+     if (value == 0 && bonjour==1) {
+       value = $(".resgauche").text()*60
+       bonjour=2
+     }
+     if (value == 0 && bonjour==2) {
+      value = $(".resdroite").text()*60
+      bonjour=1
+     }
  }, 1000);
 });
 
 // $("#cercle2").click(function() {
-//  clearInterval(timer);
-//  timer = null
+ // clearInterval(timer);
+ // timer = null
 // });
 
 // compteur gauche
@@ -24,12 +33,14 @@ $(".moinsgauche").click(function(){
   if ($(".resgauche").text() <1) {
     $(".resgauche").text(1)
   }
+  value = $(".resgauche").text()*60
 });
 
 $(".plusgauche").click(function(){
   var resgauche = $(".resgauche").text();
   resgauche = parseInt(resgauche) +1
   $(".resgauche").text(resgauche)
+  value = $(".resgauche").text()*60
 });
 
 // compteur droit
@@ -41,10 +52,31 @@ $(".moinsdroite").click(function(){
   if ($(".resdroite").text() <1) {
     $(".resdroite").text(1)
   }
+  $(".cercle").text(sec(resdroite*60));
+  value = $(".resdroite").text()*60
+  clearInterval(timer);
+  timer = null
 });
 
 $(".plusdroite").click(function(){
   var resdroite = $(".resdroite").text();
   resdroite = parseInt(resdroite) +1
   $(".resdroite").text(resdroite)
+  $(".cercle").text(sec(resdroite*60));
+  value = $(".resdroite").text()*60
+  clearInterval(timer);
+  timer = null
 });
+
+function sec(d) {
+   d = Number(d);
+   var m = Math.floor(d % 3600 / 60);
+   var s = Math.floor(d % 3600 % 60);
+   if( m <10){
+     m = "0" + m
+   }
+   if(s<10){
+     s = "0" + s
+   }
+   return m + ":" + s
+}
